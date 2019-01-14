1dc9742512e884806aaaf5ea60e7fd45f4d4bdd0
Tkyl-xy <1142085116@qq.com>
2019-01-15 00:31:55 +0800

    给display模型类models.py中verbose_name_plural起个可读的名字

--- display/admin.py
+++ display/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Display
+
 
 # Register your models here.
 #注册模块
+from .models import Display
 admin.site.register(Display)
--- display/models.py
+++ display/models.py
@@ -3,12 +3,15 @@ from django.db import models
 # Create your models here.
 class Display(models.Model):
     class Meta:
+        #这个是创建数据库表
         db_table = 'own'
+        #这个是给模型类起一个更可读的名字
+        verbose_name_plural = '作品展集'
 
     id = models.AutoField(primary_key=True)
-    description = models.CharField(default="在这里写作品的简介", max_length=100)
-    image = models.ImageField(upload_to='images/')
-    title = models.CharField(default="作品标题", max_length=50)
+    description = models.CharField(default="在这里写作品的简介", max_length=100, verbose_name='作品简介')
+    image = models.ImageField(upload_to='images/', verbose_name='图片名称')
+    title = models.CharField(default="作品标题", max_length=50, verbose_name='作品标题')
 
     #这里是实现管理标题，便于管理以后的描述标题
     def __str__(self):
